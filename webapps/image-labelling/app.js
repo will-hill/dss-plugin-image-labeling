27d0eb3b0a61a31cafda7879b9a9aacebe618f16
joachim <joachim>
2018-12-02 23:35:59 +0100

    Edited file 'webapps/image-labelling/app.js'

---
+++ webapps/image-labelling/app.js
@@ -0,0 +1,79 @@
+let categories = (getWebAppConfig().categories||[]).map(it => ({name: it.from, description: it.to}));
+let currentPath;
+
+function drawApp(allItems) {
+    if (!checkParameters()) {
+        return;
+    }
+    drawCategories(categories);
+    $('#skip').click(next)
+    next();
+}
+
+function checkParameters() {
+    console.warn(getWebAppDesc());
+    return true;
+}
+
+function drawCategories(categories) {
+    $('#category-buttons').empty();
+    $('#category-descriptions').empty();
+    categories.forEach(cat => {
+        const button = $(`<button class="cat-button">${cat.name}</button>`)
+        $('#category-buttons').append(button);
+        if (cat.description) {
+            const desc = $(`<li><strong>${cat.name}: </strong>${cat.description}</li>`)
+            $('#category-descriptions').append(desc);
+        }
+    });
+    if (categories.some(c => c.description)) {
+        $('.right').show();
+    }
+    $('#category-buttons button').each((idx, button) => {
+        $(button).click(() => { classify(categories[idx].name)})
+    });
+}
+
+function next() {
+    webappBackend.get('next', {}, updateProgress);
+}
+
+function drawItem() {
+    if (!currentPath || !currentPath.length) {
+        $('#app').html('<div id="done">The End</div>')
+    } else {
+        webappBackend.get('get-image-base64', {path: currentPath}, function(resp) {
+            let contentType = 'image/png';
+            $('#item-to-classify').html(`<img src="data:${contentType};base64,${resp.data}" />`);
+            $('#comment').val('')
+        });
+    }
+}
+
+function classify(cat) {
+    const comment = $('#comment').val()
+    webappBackend.get('classify', {path: currentPath, comment: $('#comment').val(), cat: cat}, updateProgress);
+}
+
+function updateProgress(resp) {
+    currentPath = resp.nextPath;
+    $('#total').text(resp.total);
+    $('#labelled').text(resp.labelled);
+    $('#skipped').text(resp.skipped);
+    drawItem();
+}
+
+const webappBackend = (function() {
+    function getUrl(path) {
+        return getWebAppBackendUrl(path);
+    }
+    function get(path, args, done, fail) {
+        return $.getJSON(getUrl(path), args, done, fail);
+    }
+    function post(path, args, done, fail) {
+        return $.post(getUrl(path), args, done, fail);
+    }
+    return {getUrl, get, post}
+})();
+
+drawApp();
