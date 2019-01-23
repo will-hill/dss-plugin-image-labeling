61a9deec05d970a1edf661fba974acab22c3eacd
admin <admin>
2019-01-23 16:28:42 +0100

    Edited files

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,9 @@
 var app = angular.module('labelling.setup.module', []);
 
 app.controller('LabellingSetupController', function($scope) {
-    $scope.folderParam = {"type":"FOLDER", "name":"folder", "label":"Images", "description":"Folder containing the images to label"};
-    $scope.datasetParam = {"type":"DATASET", "name":"dataset", "label":"Labels", "description":"Dataset to save the labels into"};
+    $scope.folderParam = {"type":"FOLDER", "name":"folder", "label":"Images", "description":"Folder containing the images to label", "mandatory": true, "canSelectForeign": true};
+    $scope.datasetParam = {"type":"DATASET", "name":"dataset", "label":"Labels", "description":"Dataset to save the labels into", "mandatory": true, "canSelectForeign": false};
+    $scope.categoriesParam = {"name": "categories", "type": "KEY_VALUE_LIST", "label": "Categories", "description": "Category name → optional description", "mandatory": true}
     var updateChoices = function() {
         // the parameter to callPythonDo() is passed to the do() method as the payload
         // the return value of the do() method comes back as the data parameter of the fist function()
