38dc2fcaa84e1635a1a8c1e59444f28beccd2f2a
admin <admin>
2019-03-14 19:33:06 -0400

    Edited file 'webapps/image-labeling/backend.py'

--- webapps/image-labeling/backend.py
+++ webapps/image-labeling/backend.py
@@ -60,7 +60,6 @@ def get_frame():
 @app.route('/get-image-base64')
 def get_image():
     path = request.args.get('path')
-    print('GET IMAGE: ' + path)
     with objects.get_download_stream(path) as s:
         data = b64encode(s.read())
     return json.dumps({"data": data})
