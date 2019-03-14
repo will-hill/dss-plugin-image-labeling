75caae22eb2f4f4dce4ac56254c0f4e0308115eb
admin <admin>
2019-03-14 14:48:43 -0400

    Edited file 'webapps/image-labeling/backend.py'

--- webapps/image-labeling/backend.py
+++ webapps/image-labeling/backend.py
@@ -53,6 +53,7 @@ def get_image():
     path = request.args.get('path')
     print('path: ' +str(path))
     print('determining scored image path...')
+    print(str(path))
     # scored_thumb0006.png
     # cropped_thumb0006_score_78.250237.png
     with objects.get_download_stream(path) as s:
